478fdf348e586474c4d5eee5cd063ee1a55bad41
Alexandr Kalinichenko <olkal@ciklum.com>
2021-03-12 18:39:23 +0200

    remove node modules from root

--- scrapper/src/modules/auth/routes/index.ts
+++ scrapper/src/modules/auth/routes/index.ts
@@ -9,14 +9,16 @@ authRouter.get("/login", async (req, res) => {
 });
 
 authRouter.get("/signUp", async (req, res) => {
+  console.log(req, "request");
   const data = {
     name: "test",
     email: "test@email.test",
     login: "test",
     password: "nagisa",
   };
-  const result = await authService.initRegistration(data);
-  res.send(result);
+  // const result = await authService.initRegistration(data);
+  // res.send(result);
+  res.send("200");
 });
 
 export { authRouter };
